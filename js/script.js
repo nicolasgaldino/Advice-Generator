34a9535bffa7a57c40c8d8c3b9403e6a1b484f69
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-25 20:27:09 -0300

    Conexão com à API bem sucedida.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-const url = "https://api.adviceslip.com/advice";
+const adviceTitle = document.querySelector(".adviceTitle");
+const adviceId = document.querySelector(".adviceId");
+const adviceParagraph = document.querySelector(".adviceParagraph");
+const diceBtn = document.querySelector(".diceBtn");
+
+
+const fetchAdviceAPI = async () => {
+  const url = "https://api.adviceslip.com/advice";
+  const adviceResponse = await fetch(url)
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+  return adviceResponse;
+};
+
+const renderAdvice = async () => {
+  const renderAdviceResponse = await fetchAdviceAPI();
+  adviceParagraph.innerHTML = renderAdviceResponse.slip.advice;
+};
+
+renderAdvice()
